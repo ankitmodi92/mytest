0074d98ac69699ce6cf5274db4ebc324145b9831
ankit modi <ankitmodi@ankits-MacBook-Pro.local>
2018-03-11 15:40:51 +0400

    new commit

--- run_tests.R
+++ run_tests.R
@@ -1,7 +1,6 @@
 library(testthat) 
 library(covr)
 source("~/Documents/unit_test/fibo.R")
-#test_results <- test_dir("~/Documents/unit_test/tests/", reporter="summary")
 
 cov<- file_coverage("~/Documents/unit_test/fibo.R","~/Documents/unit_test/tests/test_fibo.R")
 
