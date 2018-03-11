a96d05d71a68c12e841e2b4d8e3b76048dfe3ef3
ankit modi <ankitmodi@ankits-MacBook-Pro.local>
2018-03-11 16:57:25 +0400

    new commit

--- run_tests.R
+++ run_tests.R
@@ -1,7 +1,6 @@
 library(testthat) 
 library(covr)
 source("~/Documents/unit_test/fibo.R")
-
 cov<- file_coverage("~/Documents/unit_test/fibo.R","~/Documents/unit_test/tests/test_fibo.R")
 to_cobertura(cov, filename = "cobertura.xml")
 ## print coverage
